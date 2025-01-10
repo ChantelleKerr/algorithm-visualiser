aa6a768f22d1d506675694cfc07563fe00b4d464
ChantelleKerr <chantellekerr93@gmail.com>
2025-01-10 12:46:46 +0800

    display sorted array

--- app/sorting/page.tsx
+++ app/sorting/page.tsx
@@ -1,5 +1,30 @@
-const page = () => {
-  return <div>SORTING ALGORITHMS- Coming Soon!</div>;
+"use client";
+import { BubbleSort } from "@/algorithms/sort/bubbleSort";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+const Page = () => {
+  const [array, setArray] = useState<number[]>([6, 23, 1, 34, 5, 3, 8]);
+
+  const handleClick = () => {
+    const arrayCopy = [...array];
+    setArray(BubbleSort(arrayCopy));
+  };
+  return (
+    <>
+      <Button onClick={handleClick}>Sort</Button>
+      <div className="flex flex-row gap-2 justify-center">
+        {array.map((number, index) => (
+          <div
+            key={index}
+            className="flex justify-center items-center border w-10 h-10 md:w-20 md:h-20 "
+          >
+            {number}
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
-export default page;
+export default Page;
