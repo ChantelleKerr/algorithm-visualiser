d6cd25371161702835b2414377422f174cb12245
ChantelleKerr <chantellekerr93@gmail.com>
2025-01-29 16:00:48 +0800

    add github repo link

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,8 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <p className="w-full bg-blue flex justify-center text-xl py-2">
-      ♥ Developed by Chantelle ♥
-    </p>
+    <footer className="w-full bg-blue flex justify-between items-center text-xl py-2 px-6">
+      <p>♥ Developed by Chantelle ♥</p>
+      <Link
+        href="https://github.com/ChantelleKerr/algorithm-visualiser"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <Image
+          src="/github-mark.png"
+          alt="GitHub Repository"
+          width={30}
+          height={30}
+          className="cursor-pointer"
+        />
+      </Link>
+    </footer>
   );
 };
 
